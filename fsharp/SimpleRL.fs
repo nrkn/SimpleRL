a1c3d264a2d7d000ecbe45701d443e5e6984c753
nrkn <nrkn.com@gmail.com>
2011-02-10 15:48:25 +1300

    added attribution to fsharp implementation

--- fsharp/SimpleRL.fs
+++ fsharp/SimpleRL.fs
@@ -1,4 +1,8 @@
-﻿open System
+﻿// taken from stackoverflow:
+// http://stackoverflow.com/questions/4495993/very-simple-roguelike-in-f-making-it-more-functional
+// http://stackoverflow.com/users/17919/gradbot
+
+open System
 
 // use a third party vector class for 2D and 3D positions
 // or write your own for pratice
