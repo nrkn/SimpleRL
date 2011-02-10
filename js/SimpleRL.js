e72a5581b7c2958239d1c4bdcfb7b50896ffa1bd
Simon Oberhammer <simon@nekapuzer.at>
2011-02-10 13:01:43 +0100

    adapt to javascript coding conventions

--- js/SimpleRL.js
+++ js/SimpleRL.js
@@ -1,55 +1,52 @@
-var viewport = document.getElementById( 'viewport' );
+(function() {
+  var viewport = document.getElementById( 'viewport' );
+  var map = [
+    '####  ####', 
+    '#  ####  #', 
+    '#        #', 
+    '##      ##', 
+    ' #      # ', 
+    ' #      # ', 
+    '##      ##', 
+    '#        #', 
+    '#  ####  #', 
+    '####  ####'   
+  ];
 
-var map = [
-  '####  ####', 
-  '#  ####  #', 
-  '#        #', 
-  '##      ##', 
-  ' #      # ', 
-  ' #      # ', 
-  '##      ##', 
-  '#        #', 
-  '#  ####  #', 
-  '####  ####'   
-];
+  var x = 2;
+  var y = 2;
 
-var x, y;
-x = y = 2;
+  function tick(e) {
+    setTile(' ');
 
-var tick = function( e ) {    
-  setTile( ' ' );
+    switch(e.keyCode) {
+      case 37:
+        movePlayer(x - 1, y);
+        break;
+      case 38:
+        movePlayer(x, y - 1);
+        break;
+      case 39:
+        movePlayer(x + 1, y);
+        break;
+      case 40:
+        movePlayer(x, y + 1);
+        break;
+    }
+    setTile( '@' );
+    viewport.innerHTML = map.join('<br />');
+  };
 
-  switch( e.keyCode ) {
-    case 37:
-      movePlayer( x - 1, y );
-      break;
-    case 38:
-      movePlayer( x, y - 1 );
-      break;
-    case 39:
-      movePlayer( x + 1, y );
-      break;
-    case 40:
-      movePlayer( x, y + 1 );
-      break;
-  }
+  function setTile(tile) {
+    map[y] = map[y].substr(0, x) + tile + map[y].substr(x + 1);
+  };
 
-  setTile( '@' );
+  function movePlayer(newX, newY) {
+    if(map[ newY ][ newX ] != ' ') return;
+    x = newX;
+    y = newY;   
+  };
 
-  viewport.innerHTML = map.join( '<br />' );
-}
-
-var setTile = function( tile ) {
-  map[ y ] = map[ y ].substr( 0, x ) + tile + map[ y ].substr( x + 1 );
-}
-
-var movePlayer = function( newX, newY ) {
-  if( map[ newY ][ newX ] != ' ' ) return;
-  
-  x = newX;
-  y = newY;   
-}
-
-document.addEventListener( 'keyup', tick, 0 ); 
-
-tick( { keyCode: null } );
+  document.addEventListener('keyup', tick, false);
+  tick({keyCode: null});
+})();
