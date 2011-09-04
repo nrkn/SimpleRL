dadb8bf5648c193c12f6fb668a2b575cf7694334
Doug South <doug.south@gmail.com>
2011-09-04 19:55:58 -0400

    Replaced EXIT_ON_CLOSE with DISPOSE_ON_CLOSE to be more SWANK friendly

--- clojure/SimpleRL.clj
+++ clojure/SimpleRL.clj
@@ -20,10 +20,7 @@
 (.setFont pane (Font. "monospaced", Font/PLAIN 14))
 (.setText pane (reduce #(str %1 %2) (map #(apply str %) world)))
 (.setContentPane frame pane)
-;; SWANK really is allergic to EXIT_ON_CLOSE
-;; Uncomment if only if you don't want to restart SWANK
-;; on closing the frame...
-;(.setDefaultCloseOperation frame JFrame/EXIT_ON_CLOSE)
+(.setDefaultCloseOperation frame JFrame/DISPOSE_ON_CLOSE)
 (.pack frame)
 (.setVisible frame true)
 
@@ -35,6 +32,7 @@
 	y (deref y)
 	row (world y)]
     (assoc world y (assoc row x \@))))
+
 
 
       
