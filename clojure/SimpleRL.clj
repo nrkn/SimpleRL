bc92eab1feb174267042e7ef78f3f20c91245a87
Doug South <doug.south@gmail.com>
2011-09-04 19:49:50 -0400

    comment regarding how bad EXIT_ON_CLOSE is for SWANK

--- clojure/SimpleRL.clj
+++ clojure/SimpleRL.clj
@@ -20,6 +20,9 @@
 (.setFont pane (Font. "monospaced", Font/PLAIN 14))
 (.setText pane (reduce #(str %1 %2) (map #(apply str %) world)))
 (.setContentPane frame pane)
+;; SWANK really is allergic to EXIT_ON_CLOSE
+;; Uncomment if only if you don't want to restart SWANK
+;; on closing the frame...
 ;(.setDefaultCloseOperation frame JFrame/EXIT_ON_CLOSE)
 (.pack frame)
 (.setVisible frame true)
