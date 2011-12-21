5b54852785d9320af48b865cfe808017e5b7ab35
nrkn <nrkn.com@gmail.com>
2011-12-21 13:33:12 +1300

    js passes jslint

--- js/SimpleRL.js
+++ js/SimpleRL.js
@@ -1,52 +1,56 @@
-(function() {
-  var viewport = document.getElementById( 'viewport' );
-  var map = [
-    '####  ####', 
-    '#  ####  #', 
-    '#        #', 
-    '##      ##', 
-    ' #      # ', 
-    ' #      # ', 
-    '##      ##', 
-    '#        #', 
-    '#  ####  #', 
-    '####  ####'   
-  ];
+(function () {
+    'use strict';
 
-  var x = 2;
-  var y = 2;
+    var viewport = document.getElementById('viewport'),
+        map = [
+            '####  ####',
+            '#  ####  #',
+            '#        #',
+            '##      ##',
+            ' #      # ',
+            ' #      # ',
+            '##      ##',
+            '#        #',
+            '#  ####  #',
+            '####  ####'
+        ],
+        x = 2,
+        y = 2;
 
-  function tick(e) {
-    setTile(' ');
-
-    switch(e.keyCode) {
-      case 37:
-        movePlayer(x - 1, y);
-        break;
-      case 38:
-        movePlayer(x, y - 1);
-        break;
-      case 39:
-        movePlayer(x + 1, y);
-        break;
-      case 40:
-        movePlayer(x, y + 1);
-        break;
+    function setTile(tile) {
+        map[y] = map[y].substr(0, x) + tile + map[y].substr(x + 1);
     }
-    setTile( '@' );
-    viewport.innerHTML = map.join('<br />');
-  };
 
-  function setTile(tile) {
-    map[y] = map[y].substr(0, x) + tile + map[y].substr(x + 1);
-  };
+    function movePlayer(newX, newY) {
+        if (map[newY][newX] !== ' ') {
+            return;
+        }
+        x = newX;
+        y = newY;
+    }
 
-  function movePlayer(newX, newY) {
-    if(map[ newY ][ newX ] != ' ') return;
-    x = newX;
-    y = newY;   
-  };
+    function tick(e) {
+        setTile(' ');
 
-  document.addEventListener('keyup', tick, false);
-  tick({keyCode: null});
-})();
+        switch (e.keyCode) {
+        case 37:
+            movePlayer(x - 1, y);
+            break;
+        case 38:
+            movePlayer(x, y - 1);
+            break;
+        case 39:
+            movePlayer(x + 1, y);
+            break;
+        case 40:
+            movePlayer(x, y + 1);
+            break;
+        }
+        setTile('@');
+        viewport.innerHTML = map.join('<br />');
+    }
+
+
+    document.addEventListener('keyup', tick, false);
+    tick({keyCode: null});
+}());
