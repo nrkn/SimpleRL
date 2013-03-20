9583fd1a2b3a9c3e245d928567f45dc5c03ee96b
Radomir Dopieralski <devel@sheep.art.pl>
2013-03-20 20:36:54 +0100

    Make formatting compliant with the PEP8 standard
    - use 4 spaces for indentation instead of mix of 2 spaces and tabs
    - add PEP263 encoding specification
    - remove unneeded parentheses
    - remove unneeded empty lines, add extra empty lines where required
    - break multiple statements on a single line into multiple lines
    - rename variables

--- python/SimpleRL.py
+++ python/SimpleRL.py
@@ -1,51 +1,53 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import curses
 from curses import KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT
 
+
 MAP = [
-  '####  ####',
-  '#  ####  #',
-  '#        #',
-  '##      ##',
-  ' #      # ',
-  ' #      # ',
-  '##      ##',
-  '#        #',
-  '#  ####  #',
-  '####  ####',
+    '####  ####',
+    '#  ####  #',
+    '#        #',
+    '##      ##',
+    ' #      # ',
+    ' #      # ',
+    '##      ##',
+    '#        #',
+    '#  ####  #',
+    '####  ####',
 ]
-
 KEY_QUIT = ord('q')
-
 MOVEMENT = {
-  KEY_UP:     lambda x, y: (x, y-1),
-  KEY_DOWN:   lambda x, y: (x, y+1),
-  KEY_LEFT:   lambda x, y: (x-1, y),
-  KEY_RIGHT:  lambda x, y: (x+1, y),
+    KEY_UP: lambda x, y: (x, y-1),
+    KEY_DOWN: lambda x, y: (x, y+1),
+    KEY_LEFT: lambda x, y: (x-1, y),
+    KEY_RIGHT: lambda x, y: (x+1, y),
 }
 
+
 def main(screen):
-  x, y = 2, 2
-  
-  def draw_tile( tile ):
-    screen.addstr(y, x, tile)
+    x, y = 2, 2
 
-  def move_player( newX, newY ):
-    return (newX, newY) if ( MAP[newX][newY] == ' ') else (x, y)
+    def draw_tile(tile):
+        screen.addstr(y, x, tile)
 
-  for row in MAP: screen.addstr(row + '\n')
-  
-  key = None
-  
-  while key != KEY_QUIT:
-    draw_tile('@')
-    key = screen.getch()
+    def move_player(new_x, new_y):
+        return (new_x, new_y) if MAP[new_x][new_y] == ' ' else (x, y)
 
-    if key in [KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT]:
-      draw_tile(' ')
-      newXY = MOVEMENT[key](x, y)
-      x,y = move_player(*newXY)
+    for row in MAP:
+        screen.addstr(row + '\n')
+    key = None
+    while key != KEY_QUIT:
+        draw_tile('@')
+        key = screen.getch()
+
+        if key in [KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT]:
+            draw_tile(' ')
+            new_position = MOVEMENT[key](x, y)
+            x, y = move_player(*new_position)
+
 
 if __name__ == '__main__':
-  curses.wrapper(main)
+    curses.wrapper(main)
+
